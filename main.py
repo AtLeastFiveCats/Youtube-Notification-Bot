386068ca96a2ceae7efb1b996cd1e9dc1915a2f4
juandrzej <programming.yippee729@passinbox.com>
2025-07-25 23:15:36 +0200

    refactor variables to static when applicable and move to global scope; segregate data so it is in reasonable blocks; adjust cleaned subs data to include channel ID

--- main.py
+++ main.py
@@ -1,47 +1,52 @@
 import os
 from dotenv import load_dotenv
 
-# google api libs
+# The Google APIs Client Libraries for Python
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-# loading YT key
+# Loading YT key and setting static variables
 load_dotenv()
-YT_API_KEY: str = os.getenv("YOUTUBE_API_KEY")
-
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+YT_API_KEY: str = os.getenv("YOUTUBE_API_KEY", "")
+CLIENT_SECRETS_FILE: str = "YT.json"
+API_SERVICE_NAME: str = "youtube"
+API_VERSION: str = "v3"
+SCOPES: list[str] = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 
 def main():
-    print("Hello from youtube-notification-bot!")
-
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
-    api_service_name = "youtube"
-    api_version = "v3"
-    client_secrets_file = "YT.json"
-
     # Get credentials and create an API client
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes
+        CLIENT_SECRETS_FILE, SCOPES
     )
-
     credentials = flow.run_local_server(port=8080)
-
     youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials
+        API_SERVICE_NAME, API_VERSION, credentials=credentials
     )
 
+    # Get subscriptions list JSON response file
     request = youtube.subscriptions().list(part="snippet,contentDetails", mine=True)
     response = request.execute()
 
-    # print(response)
-    print(f"Found {len(response.get('items', []))} subscriptions")
-    for item in response.get("items", []):
-        print(f"- {item['snippet']['title']}: {item['snippet']['description']}")
+    # Prepare cleaned subs info: title / description / channel ID
+    subscriptons = response.get("items", [])
+    cleaned_subscriptions: list = []
+    print(f"Found {len(subscriptons)} subscriptions")
+    for sub in subscriptons:
+        sub_data = sub["snippet"]
+        cleaned_subscriptions.append(
+            [
+                sub_data["title"],
+                sub_data["description"],
+                sub_data["resourceId"]["channelId"],
+            ]
+        )
+    print(cleaned_subscriptions)
 
 
 if __name__ == "__main__":
