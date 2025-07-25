3e4aff804bc9af84c606eb22f08242c4914d5ca0
juandrzej <programming.yippee729@passinbox.com>
2025-07-25 18:48:20 +0200

    initialize venv with requests and dotenv; YT api key properly read on main file

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+# loading YT key
+load_dotenv()
+YT_API_KEY: str = os.getenv("YOUTUBE_API_KEY")
+
+
 def main():
+    print(YT_API_KEY)
     print("Hello from youtube-notification-bot!")
 
 
